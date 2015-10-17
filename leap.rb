9390331ff29d075236abbadd9d5b04c4bb8f64c7
Charlotte Chang <yellowspiderlove@gmail.com>
2015-10-17 15:35:13 -0400

    ordered the modulos increasing and adjust whitespace

--- leap.rb
+++ leap.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Year
-	def self.leap?(year)
-		year % 400 == 0 || year % 4 == 0 && year % 100 != 0
-	end
+  def self.leap?(year)
+	year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+  end
 end 
